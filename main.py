f46f2844ed1ba692ce06bda0ec6aa9673a30ee16
pycoder074 <elliott.kelly08@outlook.com>
2025-02-18 13:36:20 +0000

    made menu gui

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from csv_handler import *
 from utils import type_effect
 from revision import *
-from menu import menu
+from menu import MainMenu
 
 def main():
     type_effect('Hello, Elliott. What are you going to revise today? \n', speed=0.05)
-    menu()
+    menu = MainMenu()
+    menu.mainloop()
 
 main()  # Start the program
--- menu.py
+++ menu.py
@@ -1,30 +1,63 @@
+import tkinter as tk
 from revision import Revision_Controller
 from utils import type_effect
 
-def menu():
-    revision_controller = Revision_Controller()
-    while True:
-        print(f'{"-" * 30} Main Menu {"-" * 30}')
-        options = ['                                    ',
-                   '                           1. Start Revision \n',
-                   '                           2. Add a new topic \n',
-                   '                           3. Edit a topic \n',
-                   '                           4. Delete a topic \n',
-                   '                           5. Exit \n']
-        for i in options:
-            type_effect(i, speed=0.0001)
+class MainMenu(tk.Tk):
+    def __init__(self):
+        super().__init__()
 
-        choice = input('>>> ')
-        if choice == '1':
-            revision_controller.start_revision()
-        elif choice == '2':
-            revision_controller.add_new_topic()
-        elif choice == '3':
-            revision_controller.edit_topic()
-        elif choice == '4':
-            revision_controller.delete_topic()
-        elif choice == '5':
-            type_effect('Goodbye! \n', speed=0.05)
-            break  # Exit the loop and end the program
-        else:
-            type_effect('Invalid choice, please try again. \n', speed=0.05)
+        # Create Revision Controller instance
+        self.revision_controller = Revision_Controller()
+
+        # Set the title and size of the window
+        self.title("Main Menu")
+        self.geometry("400x300")
+
+        # Create labels, buttons, etc. for the GUI menu
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Start Revision Button
+        self.start_button = tk.Button(self, text="Start Revision", command=self.start_revision)
+        self.start_button.pack(pady=10)
+
+        # Add New Topic Button
+        self.add_button = tk.Button(self, text="Add New Topic", command=self.add_new_topic)
+        self.add_button.pack(pady=10)
+
+        # Edit Topic Button
+        self.edit_button = tk.Button(self, text="Edit Topic", command=self.edit_topic)
+        self.edit_button.pack(pady=10)
+
+        # Delete Topic Button
+        self.delete_button = tk.Button(self, text="Delete Topic", command=self.delete_topic)
+        self.delete_button.pack(pady=10)
+
+        # Exit Button
+        self.exit_button = tk.Button(self, text="Exit", command=self.exit_app)
+        self.exit_button.pack(pady=10)
+
+    def start_revision(self):
+        self.revision_controller.start_revision()
+        self.exit_app()
+
+    def add_new_topic(self):
+        self.revision_controller.add_new_topic()
+        self.exit_app()
+
+    def edit_topic(self):
+        self.revision_controller.edit_topic()
+        self.exit_app()
+
+    def delete_topic(self):
+        self.revision_controller.delete_topic()
+        self.exit_app()
+
+    def exit_app(self):
+        type_effect("Goodbye!\n", speed=0.05)
+        self.quit()  # Close the tkinter window
+
+# Create and run the MainMenu
+if __name__ == "__main__":
+    app = MainMenu()
+    app.mainloop()
